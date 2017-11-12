ec541cd4a8f342804e0a17653bbbc346b3d1d982
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-12 13:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(names){
-  for (let i = 1; i < names.length - 1; i++){
+  for (let i = 0; i < names.length; i++){
     console.log(`Welcome ${names[i]}!`)
   }
   return names;
